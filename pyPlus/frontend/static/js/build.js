0ad8762b97598c5cb5240f14c55eaeda18e393ca
Francisco Carrizales <fcarrizalest@gmail.com>
2014-03-28 10:50:10 -0600

    Agregamos AES para seguridad. y archivo build.js

---
+++ pyPlus/frontend/static/js/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: ".",
+    paths: {
+    	jquery: 'libs/jquery/jquery',
+        underscore: 'libs/underscore/underscore',
+        backbone: 'libs/backbone/backbone',
+        mustache: 'libs/mustache/mustache',
+        dropzone: 'vendor/dropzone.min',
+        templates: '../templates'
+    },
+    name: "main",
+    out: "main-built.js"
+})
